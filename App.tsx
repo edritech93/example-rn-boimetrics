7e94ce148779e888ecbceaed4f5c5ffa3b12e33d
Yudi Alviska <yudi.a@weefer.co.id>
2024-08-01 16:23:55 +0700

    clean code

--- App.tsx
+++ App.tsx
@@ -9,7 +9,6 @@ import {
   Alert,
 } from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import ReactNativeBiometrics, {BiometryTypes} from 'react-native-biometrics';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -37,6 +36,81 @@ export default function App() {
     email: '',
     password: '',
   });
+
+  async function _onPressSignIn() {
+    const {userId} = await verifyUserCredentials(form);
+    const rnBiometrics = new ReactNativeBiometrics();
+    const {available, biometryType} = await rnBiometrics.isSensorAvailable();
+    if (available && biometryType === BiometryTypes.FaceID) {
+      Alert.alert(
+        'Face ID',
+        'Would you like to enable Face ID authentication for the next time?',
+        [
+          {
+            text: 'Yes please',
+            onPress: async () => {
+              const {publicKey} = await rnBiometrics.createKeys();
+
+              await sendPublicKeyToServer(publicKey);
+
+              // save `userId` in the local storage to use it during Face ID authentication
+              await AsyncStorage.setItem('userId', userId);
+            },
+          },
+          {text: 'Cancel', style: 'cancel'},
+        ],
+      );
+    }
+  }
+
+  async function _onPressFaceId() {
+    const rnBiometrics = new ReactNativeBiometrics();
+    const {available, biometryType} = await rnBiometrics.isSensorAvailable();
+
+    if (!available || biometryType !== BiometryTypes.FaceID) {
+      Alert.alert('Oops!', 'Face ID is not available on this device.');
+      return;
+    }
+
+    const userId = await AsyncStorage.getItem('userId');
+
+    if (!userId) {
+      Alert.alert(
+        'Oops!',
+        'You have to sign in using your credentials first to enable Face ID.',
+      );
+      return;
+    }
+
+    const timestamp = Math.round(new Date().getTime() / 1000).toString();
+    const payload = `${userId}__${timestamp}`;
+
+    const {success, signature} = await rnBiometrics.createSignature({
+      promptMessage: 'Sign in',
+      payload,
+    });
+
+    if (!success) {
+      Alert.alert(
+        'Oops!',
+        'Something went wrong during authentication with Face ID. Please try again.',
+      );
+      return;
+    }
+
+    const {status, message} = await verifySignatureWithServer({
+      signature,
+      payload,
+    });
+
+    if (status !== 'success') {
+      Alert.alert('Oops!', message);
+      return;
+    }
+
+    Alert.alert('Success!', 'You are successfully authenticated!');
+  }
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#e8ecf4'}}>
       <View style={styles.container}>
@@ -46,16 +120,15 @@ export default function App() {
           </View>
 
           <Text style={styles.title}>
-            Welcome to <Text style={{color: '#0742fc'}}>RealApps</Text>
+            Example <Text style={{color: '#0742fc'}}>Face ID</Text>
           </Text>
 
-          <Text style={styles.subtitle}>Collaborate with your friends</Text>
+          <Text style={styles.subtitle}>Test login here...</Text>
         </View>
 
         <View style={styles.form}>
           <View style={styles.input}>
             <Text style={styles.inputLabel}>Email address</Text>
-
             <TextInput
               autoCapitalize="none"
               autoCorrect={false}
@@ -84,111 +157,14 @@ export default function App() {
 
           <View style={styles.formAction}>
             <TouchableOpacity
-              onPress={async () => {
-                const {userId} = await verifyUserCredentials(form);
-
-                // handle onPress
-                const rnBiometrics = new ReactNativeBiometrics();
-
-                const {available, biometryType} =
-                  await rnBiometrics.isSensorAvailable();
-
-                if (available && biometryType === BiometryTypes.FaceID) {
-                  Alert.alert(
-                    'Face ID',
-                    'Would you like to enable Face ID authentication for the next time?',
-                    [
-                      {
-                        text: 'Yes please',
-                        onPress: async () => {
-                          const {publicKey} = await rnBiometrics.createKeys();
-
-                          await sendPublicKeyToServer(publicKey);
-
-                          // save `userId` in the local storage to use it during Face ID authentication
-                          await AsyncStorage.setItem('userId', userId);
-                        },
-                      },
-                      {text: 'Cancel', style: 'cancel'},
-                    ],
-                  );
-                }
-              }}>
-              <View style={styles.btn}>
-                <Text style={styles.btnText}>Sign in</Text>
-              </View>
+              style={styles.btn}
+              onPress={() => _onPressSignIn()}>
+              <Text style={styles.btnText}>Sign in</Text>
             </TouchableOpacity>
-
-            <View style={styles.formActionSpacer} />
-
             <TouchableOpacity
-              onPress={async () => {
-                const rnBiometrics = new ReactNativeBiometrics();
-                const {available, biometryType} =
-                  await rnBiometrics.isSensorAvailable();
-
-                if (!available || biometryType !== BiometryTypes.FaceID) {
-                  Alert.alert(
-                    'Oops!',
-                    'Face ID is not available on this device.',
-                  );
-                  return;
-                }
-
-                const userId = await AsyncStorage.getItem('userId');
-
-                if (!userId) {
-                  Alert.alert(
-                    'Oops!',
-                    'You have to sign in using your credentials first to enable Face ID.',
-                  );
-                  return;
-                }
-
-                const timestamp = Math.round(
-                  new Date().getTime() / 1000,
-                ).toString();
-                const payload = `${userId}__${timestamp}`;
-
-                const {success, signature} = await rnBiometrics.createSignature(
-                  {
-                    promptMessage: 'Sign in',
-                    payload,
-                  },
-                );
-
-                if (!success) {
-                  Alert.alert(
-                    'Oops!',
-                    'Something went wrong during authentication with Face ID. Please try again.',
-                  );
-                  return;
-                }
-
-                const {status, message} = await verifySignatureWithServer({
-                  signature,
-                  payload,
-                });
-
-                if (status !== 'success') {
-                  Alert.alert('Oops!', message);
-                  return;
-                }
-
-                Alert.alert('Success!', 'You are successfully authenticated!');
-              }}>
-              <View style={styles.btnSecondary}>
-                <MaterialCommunityIcons
-                  color="#000"
-                  name="face-recognition"
-                  size={22}
-                  style={{marginRight: 12}}
-                />
-
-                <Text style={styles.btnSecondaryText}>Face ID</Text>
-
-                <View style={{width: 34}} />
-              </View>
+              style={styles.btnSecondary}
+              onPress={() => _onPressFaceId()}>
+              <Text style={styles.btnSecondaryText}>Face ID</Text>
             </TouchableOpacity>
           </View>
 
@@ -245,6 +221,7 @@ const styles = StyleSheet.create({
   },
   formAction: {
     marginVertical: 24,
+    gap: 16,
   },
   formActionSpacer: {
     marginVertical: 8,
